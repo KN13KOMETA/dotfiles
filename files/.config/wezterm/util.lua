178837da527cee26a4d6c2b5ee25778139853fb4
KN13KOMETA <kn13kometa@gmail.com>
2025-10-27 19:34:45 +0300

    Add table_merge util

--- files/.config/wezterm/util.lua
+++ files/.config/wezterm/util.lua
@@ -1,7 +1,29 @@
 local M = {}
 
+local MAX_RECURSIVE_DEPTH = 69
+
 M.basename = function(s)
   return string.gsub(s, "(.*[/\\])(.*)", "%2")
+end
+
+M.table_merge = function(t1, t2)
+  local function merge(dst, src, depth)
+    if depth > MAX_RECURSIVE_DEPTH then
+      return dst
+    end
+
+    for k, v in pairs(src) do
+      if type(v) == "table" then
+        dst[k] = type(dst[k]) == "table" and dst[k] or {}
+        merge(dst[k], v, depth + 1)
+      else
+        dst[k] = v
+      end
+    end
+    return dst
+  end
+
+  return merge(t1, t2, 0)
 end
 
 return M
