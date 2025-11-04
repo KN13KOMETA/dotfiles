94f85686eb7ef78c362fb1c82e9ce926bd0c6d4a
KN13KOMETA <kn13kometa@gmail.com>
2025-11-04 19:58:13 +0300

    Zinit, zsh and fzf

--- files/.config/home-manager/home.nix
+++ files/.config/home-manager/home.nix
@@ -20,6 +20,7 @@
   home.packages = [
     pkgs.fastfetch
     pkgs.neovim
+    pkgs.zinit
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -44,7 +45,44 @@
   };
 
   programs = {
-    zsh = { enable = true; };
+    zsh = {
+      enable = true;
+
+      initContent = ''
+        # Default zinit location
+        ZINIT_HOME="''${XDG_DATA_HOME:-''${HOME}/.local/share}/zinit/zinit.git"
+
+        # Source/Load zinit
+        source "''${ZINIT_HOME}/zinit.zsh"
+
+        # Add in zsh plugins
+        zinit light zsh-users/zsh-syntax-highlighting
+        zinit light zsh-users/zsh-completions
+        zinit light zsh-users/zsh-autosuggestions
+        zinit light Aloxaf/fzf-tab
+
+        # Add in snippets
+        zinit snippet OMZP::git
+        zinit snippet OMZP::sudo
+        zinit snippet OMZP::archlinux
+        zinit snippet OMZP::aws
+        zinit snippet OMZP::kubectl
+        zinit snippet OMZP::kubectx
+        zinit snippet OMZP::command-not-found
+
+        # Load completions
+        autoload -Uz compinit && compinit
+
+        zinit cdreplay -q
+
+        # Completion styling
+        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+        zstyle ':completion:*' menu no
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+        zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+      '';
+    };
     oh-my-posh = {
       enable = true;
       enableZshIntegration = true;
@@ -54,6 +92,10 @@
       enable = true;
       enableZshIntegration = true;
       options = [ "--cmd cd" ];
+    };
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
     };
   };
 
@@ -88,9 +130,6 @@
   #
   #  /etc/profiles/per-user/kometa13/etc/profile.d/hm-session-vars.sh
   #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
