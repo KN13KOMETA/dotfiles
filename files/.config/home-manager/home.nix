95f4d1bb6e537bab55e73e7a709bd44b96642ad5
KN13KOMETA <kn13kometa@gmail.com>
2025-11-04 20:14:20 +0300

    Do not highlight pasted text

--- files/.config/home-manager/home.nix
+++ files/.config/home-manager/home.nix
@@ -94,6 +94,9 @@
         zstyle ':completion:*' menu no
         zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
         zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+        # Remove highlight of pasted text
+        zle_highlight+=(paste:none)
       '';
     };
     oh-my-posh = {
