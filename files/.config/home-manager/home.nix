b3ea3efb75dfb90429c9d40e614abd42cabd7e74
KN13KOMETA <kn13kometa@gmail.com>
2026-02-07 17:51:05 +0300

    Move programs out of programs = {}

--- files/.config/home-manager/home.nix
+++ files/.config/home-manager/home.nix
@@ -110,84 +110,85 @@
   #   defaultEditor = true;
   # };
 
-  programs = {
-    zsh = {
-      enable = true;
+  programs.zsh = {
+    enable = true;
 
-      history = {
-        size = 6666;
+    history = {
+      size = 6666;
 
-        append = true;
-        share = true;
-        ignoreSpace = true;
+      append = true;
+      share = true;
+      ignoreSpace = true;
 
-        ignoreAllDups = true;
-        ignoreDups = true;
-        saveNoDups = true;
-        findNoDups = true;
-      };
-
-      # TODO: wait till zinit appears in home manager
-      # TODO: mayber switch bindkeys
-      initContent = ''
-        # Default zinit location
-        ZINIT_HOME="''${XDG_DATA_HOME:-''${HOME}/.local/share}/zinit/zinit.git"
-
-        # Source/Load zinit
-        source "''${ZINIT_HOME}/zinit.zsh"
-
-        # Add in zsh plugins
-        zinit light zsh-users/zsh-syntax-highlighting
-        zinit light zsh-users/zsh-completions
-        zinit light zsh-users/zsh-autosuggestions
-        zinit light Aloxaf/fzf-tab
-
-        # Add in snippets
-        zinit snippet OMZP::git
-        zinit snippet OMZP::sudo
-        zinit snippet OMZP::archlinux
-        zinit snippet OMZP::aws
-        zinit snippet OMZP::kubectl
-        zinit snippet OMZP::kubectx
-        zinit snippet OMZP::command-not-found
-
-        # Load completions
-        autoload -Uz compinit && compinit
-
-        zinit cdreplay -q
-
-        # Completion styling
-        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
-        zstyle ':completion:*' menu no
-        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-        zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
-
-        # Remove highlight of pasted text
-        zle_highlight+=(paste:none)
-
-        # Keybindings
-        bindkey -e
-        bindkey '^[[A' history-search-backward
-        bindkey '^p' history-search-backward
-        bindkey '^[[B' history-search-forward
-        bindkey '^n' history-search-forward
-        bindkey '^[w' kill-region
-      '';
+      ignoreAllDups = true;
+      ignoreDups = true;
+      saveNoDups = true;
+      findNoDups = true;
     };
-    oh-my-posh = {
-      enable = true;
-      enableZshIntegration = true;
-      configFile = "${config.xdg.configHome}/oh-my-posh/custom.toml";
-    };
-    zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-      options = [ "--cmd cd" ];
-    };
-    fzf = {
-      enable = true;
-      enableZshIntegration = true;
-    };
+
+    # TODO: wait till zinit appears in home manager
+    # TODO: mayber switch bindkeys
+    initContent = ''
+      # Default zinit location
+      ZINIT_HOME="''${XDG_DATA_HOME:-''${HOME}/.local/share}/zinit/zinit.git"
+
+      # Source/Load zinit
+      source "''${ZINIT_HOME}/zinit.zsh"
+
+      # Add in zsh plugins
+      zinit light zsh-users/zsh-syntax-highlighting
+      zinit light zsh-users/zsh-completions
+      zinit light zsh-users/zsh-autosuggestions
+      zinit light Aloxaf/fzf-tab
+
+      # Add in snippets
+      zinit snippet OMZP::git
+      zinit snippet OMZP::sudo
+      zinit snippet OMZP::archlinux
+      zinit snippet OMZP::aws
+      zinit snippet OMZP::kubectl
+      zinit snippet OMZP::kubectx
+      zinit snippet OMZP::command-not-found
+
+      # Load completions
+      autoload -Uz compinit && compinit
+
+      zinit cdreplay -q
+
+      # Completion styling
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+      zstyle ':completion:*' menu no
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+      zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+      # Remove highlight of pasted text
+      zle_highlight+=(paste:none)
+
+      # Keybindings
+      bindkey -e
+      bindkey '^[[A' history-search-backward
+      bindkey '^p' history-search-backward
+      bindkey '^[[B' history-search-forward
+      bindkey '^n' history-search-forward
+      bindkey '^[w' kill-region
+    '';
+  };
+
+  programs.oh-my-posh = {
+    enable = true;
+    enableZshIntegration = true;
+    configFile = "${config.xdg.configHome}/oh-my-posh/custom.toml";
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    options = [ "--cmd cd" ];
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
   };
 }
