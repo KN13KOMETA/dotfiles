c33e12d0f4ba9b8ebaac4c3bceb36196bac97789
KN13KOMETA <kn13kometa@gmail.com>
2026-02-07 16:08:14 +0300

    Move username, homeDirectory and stateVersion in home = {}

--- files/.config/home-manager/home.nix
+++ files/.config/home-manager/home.nix
@@ -1,19 +1,16 @@
-{ config, pkgs, ... }:
-
-{
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "kometa13";
-  home.homeDirectory = "/home/kometa13";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+{ config, pkgs, ... }: {
+  home = {
+    username = "kometa13";
+    homeDirectory = "/home/kometa13";
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "25.05";
+  };
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
