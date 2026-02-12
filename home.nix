54db4ce15088cbe0199d0bbfc1efd8385a697a35
KN13KOMETA <kn13kometa@gmail.com>
2026-02-12 23:03:34 +0300

    Add wezterm to home.file

--- home.nix
+++ home.nix
@@ -11,19 +11,12 @@ in
   # Check hm release notes before changing
   home.stateVersion = "25.11";
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    # TODO: Write wezterm config
+    ".config/wezterm" = {
+      source = ./files/.config/wezterm;
+      recursive = true;
+    };
   };
 
   home.sessionVariables = {
