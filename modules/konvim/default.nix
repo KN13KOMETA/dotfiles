b0d2b905c626225cf8d43b9470ba3be443d0cbba
KN13KOMETA <kn13kometa@gmail.com>
2026-02-20 23:55:59 +0300

    Add some options

--- modules/konvim/default.nix
+++ modules/konvim/default.nix
@@ -1,5 +1,28 @@
 {
   config.vim = {
-    # theme.enable = true;
+    clipboard = {
+      enable = true;
+      registers = "unnamedplus";
+    };
+
+    options = {
+      # TODO: sort this
+      cursorlineopt = "number";
+
+      mouse = "a";
+
+      shiftwidth = 0;
+      tabstop = 2;
+
+      # TODO: dynamic wrap
+      wrap = false;
+
+      cursorline = true;
+
+      ignorecase = true;
+      smartcase = true;
+
+      guifont = "IntoneMono NF:h11.2";
+    };
   };
 }
